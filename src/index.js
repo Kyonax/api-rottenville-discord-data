c69e3e1ae0c32c5fef88f66ebfff4712fe192045
Kyonax <kyonax25@gmail.com>
2022-06-18 15:48:48 -0500

    progress: Init of API
    Setting up all the interface of the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const app = require('express')();
-const PORT = 8080;
+const Exe = require('../tools/functions')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + "public"))
 
 app.listen(PORT, () => {
     console.log(`Let's Go!! http://localhost:${PORT}`)
 });
 
-
-app.get('/', (req, res) => {    
-    res.send("Re Testing")
-})
+/**
+app.get('/', async (req, res) => {
+    let json = await Exe.readJSON("./data/Members.json")
+    res.send(JSON.parse(json).data)
+}) */
